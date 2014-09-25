4f0f09b6b699cc585db31877b1b781ecd824a238
Eric Shemtov <shemtov.eric@gmail.com>
2014-09-25 16:36:46 -0400

    fixed seed file, changed db to pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140829012721) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: true do |t|
     t.string   "team1"
     t.string   "team2"
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,29 +3,28 @@ require 'faker'
 a = [true, false]
 a.sample
 
-20.times do 
+5.times do 
 	User.create(name: Faker::Name.name, email: Faker::Internet.email, password: 'eric', tier: 0)
 end
 
 User.create(name: "Admin", email: "admin@admin.com", password: "eric", tier: 1)
 
-6.times do 
+2.times do 
 	Game.create(team1: "Jets", team2: Faker::Address.state, spread: rand(-3..3), week: rand(1..3) )
 end
 
 3.times do
-	Pick.create(selection: Faker::Address.state, week: rand(1..3), winner: a.sample, user_id: 2 )
+	Pick.create(selection: (Game.last.team2), week: rand(1..3), winner: a.sample, user_id: 2, game_id: 1)
 end
 
 3.times do
-	Pick.create(selection: Faker::Address.state, week: rand(1..3), winner: a.sample, user_id: 3 )
+	Pick.create(selection: (Game.last.team1), week: rand(1..3), winner: a.sample, user_id: 3, game_id: 2)
 end
 
 3.times do
-	Pick.create(selection: Faker::Address.state, week: rand(1..3), winner: a.sample, user_id: 4 )
+	Pick.create(selection: (Game.first.team2), week: rand(1..3), winner: a.sample, user_id: 4, game_id: 2)
 end
-
 
 10.times do
-	Pick.create(selection: "Jets", week: rand(1..3), winner: a.sample )
+	Pick.create(selection: (Game.first.team1), week: rand(1..3), winner: a.sample, game_id: 2 )
 end
